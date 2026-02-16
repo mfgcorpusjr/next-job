3365a2bf767dcaaa920ba15cf3498331dacc2c42
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-02-16 20:56:21 +0800

    Get statistics and chart data

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,9 +1,14 @@
 import { Metadata } from "next";
 
+import { getStatisticsData, getChartData } from "@/features/job/utils/actions";
+
 export const metadata: Metadata = {
   title: "Dashboard",
 };
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const statisticsData = await getStatisticsData();
+  const chartData = await getChartData();
+
   return <div>DashboardPage</div>;
 }
--- src/features/job/utils/actions.ts
+++ src/features/job/utils/actions.ts
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import { subMonths, startOfMonth, format, eachMonthOfInterval } from "date-fns";
 
 import prisma from "@/lib/prisma";
 import {
@@ -53,4 +54,75 @@ export const upsertJob = async ({ id = "", formData }: upsertJobProps) => {
   } catch (e) {
     return renderError(e);
   }
+};
+
+export const getStatisticsData = async () => {
+  const userId = await getUserIdOrRedirect();
+
+  const data = await prisma.job.groupBy({
+    by: ["status"],
+    _count: {
+      status: true,
+    },
+    where: {
+      clerkId: userId,
+    },
+  });
+
+  const statisticsData = data.reduce(
+    (acc, item) => {
+      acc[item.status] = item._count.status;
+
+      return acc;
+    },
+    {} as { [key: string]: number },
+  );
+
+  return {
+    Pending: 0,
+    Interview: 0,
+    Declined: 0,
+    ...statisticsData,
+  };
+};
+
+export const getChartData = async () => {
+  const userId = await getUserIdOrRedirect();
+
+  const now = new Date();
+  const sixMonthsAgo = startOfMonth(subMonths(now, 5));
+
+  const data = await prisma.job.findMany({
+    where: {
+      clerkId: userId,
+      createdAt: { gte: sixMonthsAgo },
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  const monthInterval = eachMonthOfInterval({
+    start: sixMonthsAgo,
+    end: now,
+  });
+
+  const groups: Record<string, number> = {};
+  monthInterval.forEach((date) => {
+    groups[format(date, "yyyy-MM")] = 0;
+  });
+
+  data.forEach((d) => {
+    const key = format(d.createdAt, "yyyy-MM");
+    if (groups[key] !== undefined) {
+      groups[key]++;
+    }
+  });
+
+  const chartData = Object.entries(groups).map(([month, count]) => ({
+    month,
+    count,
+  }));
+
+  return chartData;
 };
