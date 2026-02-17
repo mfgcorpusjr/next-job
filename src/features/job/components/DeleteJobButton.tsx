99272d343b166ef89a6b9dc50fa40a8e2125beba
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-02-17 11:10:52 +0800

    Delete job

--- src/features/job/components/DeleteJobButton.tsx
+++ src/features/job/components/DeleteJobButton.tsx
@@ -1,13 +1,49 @@
+"use client";
+
+import { useTransition } from "react";
+import { toast } from "sonner";
+
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import DeleteDialog from "@/components/DeleteDialog";
+
+import { deleteJob } from "@/features/job/utils/actions";
 
 type DeleteJobButtonProps = {
   id: string;
 };
 
 export default function DeleteJobButton({ id }: DeleteJobButtonProps) {
+  const [isPending, startTransition] = useTransition();
+
+  const handleDeleteJob = async () => {
+    startTransition(async () => {
+      const { status, message } = await deleteJob(id);
+
+      if (status === "SUCCESS") {
+        toast.success(message);
+      } else {
+        toast.error(message);
+      }
+    });
+  };
+
   return (
-    <Button variant="outline" size="sm" className="flex-1 cursor-pointer">
-      Delete
-    </Button>
+    <DeleteDialog
+      trigger={
+        <Button variant="outline" size="sm" className="flex-1 cursor-pointer">
+          Delete
+        </Button>
+      }
+    >
+      <Button
+        variant="destructive"
+        disabled={isPending}
+        onClick={handleDeleteJob}
+      >
+        {isPending && <Spinner data-icon="inline-start" />}
+        Delete
+      </Button>
+    </DeleteDialog>
   );
 }
